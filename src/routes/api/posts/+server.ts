e4ec102ea6f5c5c5f30a144e016e59bfb59b1713
jojobii <jojobii.arks@gmail.com>
2023-04-28 15:22:44 -0700

    wip: basic post api call

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -1,5 +1,21 @@
 import type { RequestHandler } from './$types';
+import supabaseServiceRoleClient from '$lib/clients/supabase.server';
+import { json } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async () => {
-	return new Response();
+	try {
+		const { data, error } = await supabaseServiceRoleClient
+			.from('posts')
+			.select('*')
+			.order('created_at', { ascending: false });
+		if (error) {
+			throw error;
+		}
+		return json({ data });
+	} catch (error) {
+		if (error instanceof Error) {
+			return json({ error: error.message }, { status: 500 });
+		}
+		return json({ error: 'Unknown error' }, { status: 500 });
+	}
 };
