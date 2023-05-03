852b14edd7806102d600ca08f019942a6ee1c0e2
jojobii <jojobii.arks@gmail.com>
2023-05-03 06:42:59 -0700

    wip: remove email login

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -18,29 +18,29 @@
 	{#if !data.session}
 		<!-- ? Handle login -->
 
-		<div class="rounded-xl px-8 py-6 drop-shadow-sm max-w-sm mx-auto border">
+		<div class="rounded-xl px-8 py-6 drop-shadow-sm max-w-md mx-auto border">
 			<Auth
 				supabaseClient={data.supabase}
 				appearance={{ theme: ThemeSupa }}
 				providers={['discord']}
 				redirectTo={dev ? 'http://localhost:5173' : undefined}
+				onlyThirdPartyProviders={true}
 			/>
 			<p class="text-center text-sm">
-				<b>Your account is tied to your email.</b>
-				<br />
-				<i
-					>If you sign in with Discord, it will use the email associated with your Discord account.</i
-				>
+				<i>Your account is tied to the email used in your Discord account.</i>
 			</p>
 		</div>
 	{:else}
 		<!-- ? Handle profile customization -->
 
-		<div class="rounded-xl px-8 py-6 drop-shadow-sm max-w-sm mx-auto border">
+		<div class="rounded-xl px-8 py-6 drop-shadow-sm max-w-md mx-auto border">
 			<h2 class="text-xl font-bold mb-2">Profile</h2>
 			<p class="text-sm mb-4">
 				<b>Username:</b>
 				{data.profile?.name}
+				<br />
+				<b>Discord:</b>
+				{data.session.user.user_metadata.name}
 				<br />
 				<b>Email:</b>
 				{data.session.user.email}
