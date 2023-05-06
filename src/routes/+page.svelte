18c7cd3155a886ae9880d9ad86542a1d328a0820
jojobii <jojobii.arks@gmail.com>
2023-05-05 17:27:14 -0700

    wip: giarejfhgbnlkaehjdrfbgae

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,14 +6,8 @@
 
 <main class="max-w-3xl mx-auto my-8">
 	<h1 class="font-black text-4xl">Home</h1>
-	<a class="btn" href="/profile">Account</a>
+	<a class="btn" href="/auth">Account</a>
 	<a class="btn" href="/preview">Preview</a>
 	<a class="btn" href="/upload">Upload</a>
-	<h3 class="mb-8">
-		{#if data.session}
-			Welcome, {data.session.user.id}
-		{:else}
-			Welcome, guest
-		{/if}
-	</h3>
+	<h3 class="mb-8">Welcome, guest</h3>
 </main>
