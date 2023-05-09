08534ee02e99f57a0df858717090259f18eba535
jojobii <jojobii.arks@gmail.com>
2023-05-09 10:27:59 -0700

    feat: add svelte-seo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,26 @@
 	import '../app.css';
 
 	import AppShell from './AppShell.svelte';
+	import SvelteSeo from 'svelte-seo';
 </script>
+
+<SvelteSeo
+	title="Symbol Bucket"
+	description="Upload and share your Symbol Arts from PSO2!"
+	openGraph={{
+		title: 'Symbol Bucket',
+		description: 'Upload and share your Symbol Arts from PSO2!',
+		url: 'https://symbol.arks.cafe',
+		images: [{ url: 'https://symbols.arks.moe/og-image.png' }]
+	}}
+	twitter={{
+		card: 'summary',
+		site: '@arks_cafe',
+		title: 'Symbol Bucket',
+		description: 'Upload and share your Symbol Arts from PSO2!',
+		image: 'https://symbols.arks.moe/og-image.png'
+	}}
+/>
 
 <AppShell>
 	<slot />
