16fb05f02c32552f398c06e9558834b71cd9d739
jojobii <jojobii.arks@gmail.com>
2023-05-11 18:41:25 -0700

    test: oops @ simpleNavigation.e2e.ts

--- tests/e2e/simpleNavigation.e2e.ts
+++ tests/e2e/simpleNavigation.e2e.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-	await page.goto('http://localhost:5173/');
+	await page.goto('/');
 	await page.getByRole('link', { name: 'Upload' }).click();
 	await expect(page.getByText('You must be signed in to upload.')).toBeVisible();
 	// todo: figure out how to upload a file
