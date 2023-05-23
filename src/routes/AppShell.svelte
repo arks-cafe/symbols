b55ff036639aacb9fe2f28616127f9f9e43117e7
jojobii <jojobii.arks@gmail.com>
2023-05-23 07:40:40 -0700

    style: run formatter and fix prettierignore

--- src/routes/AppShell.svelte
+++ src/routes/AppShell.svelte
@@ -1,4 +1,4 @@
-<main class="container mx-auto max-w-6xl min-h-screen bg-base-100 shadow-lg pb-4">
+<main class="container mx-auto min-h-screen max-w-6xl bg-base-100 pb-4 shadow-lg">
 	<header class="sticky top-4 z-40 mb-8 px-8">
 		<div class="mx-auto max-w-5xl rounded-xl bg-primary bg-opacity-90 p-2 text-primary-content">
 			<a href="/" class="btn-ghost btn overflow-x-clip text-4xl font-black normal-case">
