53a354b24c0b3ee2971de19ed2854f7f9ac60fab
jojobii <jojobii.arks@gmail.com>
2023-05-23 07:38:14 -0700

    fix: app shell min height

--- src/routes/AppShell.svelte
+++ src/routes/AppShell.svelte
@@ -1,15 +1,12 @@
-<div class="bg-base-300">
-	<main class="container mx-auto min-h-screen max-w-6xl bg-base-100 shadow-lg">
-		<header class="sticky top-4 z-40 mb-8 px-8">
-			<div class="mx-auto max-w-5xl rounded-xl bg-primary bg-opacity-90 p-2 text-primary-content">
-				<a href="/" class="btn-ghost btn overflow-x-clip text-4xl font-black normal-case">
-					<h1 class="">Symbol Bucket</h1>
-				</a>
-			</div>
-		</header>
-
-		<div class="px-8">
-			<slot />
+<main class="container mx-auto max-w-6xl min-h-screen bg-base-100 shadow-lg pb-4">
+	<header class="sticky top-4 z-40 mb-8 px-8">
+		<div class="mx-auto max-w-5xl rounded-xl bg-primary bg-opacity-90 p-2 text-primary-content">
+			<a href="/" class="btn-ghost btn overflow-x-clip text-4xl font-black normal-case">
+				<h1 class="">Symbol Bucket</h1>
+			</a>
 		</div>
-	</main>
-</div>
+	</header>
+	<div class="px-8">
+		<slot />
+	</div>
+</main>
